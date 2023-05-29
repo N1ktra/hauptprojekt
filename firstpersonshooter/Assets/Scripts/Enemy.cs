1bc50ad97e3bf1dacb33e4c398309c06115e44ec
lennert93 <lennert.hagmann@tu-dortmund.de>
2023-05-29 12:53:14 +0200

    worked on enemy moving, aber noch nicht smooth

--- firstpersonshooter/Assets/Scripts/Enemy.cs
+++ firstpersonshooter/Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
@@ -29,30 +30,54 @@ public class Enemy : MonoBehaviour
         healthBar.maxValue = maxHealth;
         currentHealth = maxHealth;
         UpdateHealthBar();
+        StartCoroutine(Repeater());
+    }
+
+    private IEnumerator Repeater()
+    {
+        while (true)
+        {
+
+            yield return new WaitForSeconds(3);
+            move();
+        }
     }
 
 
     public void Update()
     {
         healthBar.transform.rotation = Quaternion.LookRotation(transform.position - cam.transform.position);
-        //if (path.Count==0) // || getDistanceBetween2Vectors(GameObject.FindGameObjectWithTag("Player").transform.position, transform.position) > 30
-        //{
-        //    Debug.Log("test");
-        //    path = pathfinding.AStar(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position);
-        //}
-        //move();
-
+        if (path.Count==0) // || getDistanceBetween2Vectors(GameObject.FindGameObjectWithTag("Player").transform.position, transform.position) > 30
+        {
+            Debug.Log("test");
+            path = pathfinding.AStar(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position);
+        }
     }
+
+    public List<Node> getPath()
+    {
+        if(path == null) { return null; }
+        else { return path; }
+    }
+
+    private float counter;
 
     //moves one node and deletes it from path List
     private void move()
     {
-        if (path.Count >= 1) { 
+        if (path.Count >= 1) {
             Node next = path.First();
             path.RemoveAt(0);
-            this.GetComponent<Rigidbody>().transform.position += vectorFromTo(transform.position, next.worldPosition) *  Time.fixedDeltaTime;
+            transform.position += vectorFromTo(transform.position, addY(next.worldPosition));
+            //this.GetComponent<Rigidbody>().transform.position += vectorFromTo(transform.position, addY(next.worldPosition)) *  Time.fixedDeltaTime;
             Debug.Log("bewegt");
         }
+    }
+
+    private Vector3 addY(Vector3 pos)
+    {
+        Vector3 added = new Vector3(pos.x, pos.y + 1, pos.z);
+        return added;
     }
 
     private Vector3 vectorFromTo(Vector3 start, Vector3 end)
